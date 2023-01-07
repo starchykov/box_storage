e7518c999f05ed547b57906278e3afa033d45db3
Ivan Starchykov <48174604+starchykov@users.noreply.github.com>
2023-01-08 01:04:04 +0200

    Categories screen improvements

--- lib/ui/screens/categories_screen/categories_screen.dart
+++ lib/ui/screens/categories_screen/categories_screen.dart
@@ -10,43 +10,48 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: [
-          CupertinoSliverNavigationBar(
-            border: null,
-            backgroundColor: CupertinoColors.systemBackground.withOpacity(0),
-            trailing: CupertinoButton(
-              minSize: 0,
-              padding: EdgeInsets.zero,
-              onPressed: () {},
-              child: const Icon(CupertinoIcons.add),
+      child: SafeArea(
+        top: false,
+        child: CustomScrollView(
+          slivers: [
+            CupertinoSliverNavigationBar(
+              border: null,
+              backgroundColor: CupertinoColors.systemBackground.withOpacity(0),
+              trailing: CupertinoButton(
+                minSize: 0,
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+                child: const Icon(CupertinoIcons.add),
+              ),
+              largeTitle: const Text('Categories'),
             ),
-            largeTitle: const Text('Categories'),
-          ),
-          SliverFillRemaining(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                SizedBox(
-                  height: 100.0,
-                  width: double.maxFinite,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: const [
-                      _AddCategory(),
-                      _Categories(),
-                      _Categories(),
-                      _Categories(),
-                    ],
+            SliverFillRemaining(
+              hasScrollBody: false,
+              fillOverscroll: false,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    height: 100.0,
+                    width: double.maxFinite,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: const [
+                        _AddCategory(),
+                        _Categories(),
+                        _Categories(),
+                        _Categories(),
+                      ],
+                    ),
                   ),
-                ),
-                const Spacer(),
-                const Text('No data'),
-                const Spacer(),
-              ],
+                  const Expanded(
+                    child: _ScanCode(),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -113,6 +118,158 @@ class _Categories extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _ScanCode extends StatelessWidget {
+  const _ScanCode({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                  color: CupertinoColors.secondarySystemFill,
+                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              SizedBox(
+                height: 195,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'S: '.toUpperCase(),
+                        style: CupertinoTheme.of(context)
+                            .textTheme
+                            .pickerTextStyle
+                            .merge(TextStyle(color: CupertinoColors.label.withOpacity(.5))),
+                        children: [
+                          TextSpan(
+                            text: 'A',
+                            style: CupertinoTheme.of(context).textTheme.pickerTextStyle.merge(
+                                  TextStyle(
+                                    color: CupertinoColors.label.withOpacity(.5),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 12.0),
+                    RichText(
+                      text: TextSpan(
+                        text: 'N: '.toUpperCase(),
+                        style: CupertinoTheme.of(context)
+                            .textTheme
+                            .pickerTextStyle
+                            .merge(TextStyle(color: CupertinoColors.label.withOpacity(.5))),
+                        children: [
+                          TextSpan(
+                            text: '12345 ',
+                            style: CupertinoTheme.of(context).textTheme.pickerTextStyle.merge(
+                                  TextStyle(
+                                    color: CupertinoColors.label.withOpacity(.5),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 12.0),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Category:',
+                          style: CupertinoTheme.of(context)
+                              .textTheme
+                              .textStyle
+                              .merge(TextStyle(color: CupertinoColors.label.withOpacity(.5))),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 8),
+                          padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                          decoration: const BoxDecoration(
+                            color: CupertinoColors.separator,
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Name',
+                              style: CupertinoTheme.of(context)
+                                  .textTheme
+                                  .tabLabelTextStyle
+                                  .merge(const TextStyle(color: CupertinoColors.white)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Description:',
+                          style: CupertinoTheme.of(context)
+                              .textTheme
+                              .textStyle
+                              .merge(TextStyle(color: CupertinoColors.label.withOpacity(.5))),
+                        ),
+                        Text(
+                          'Lorem Ipsum is simply dummy ',
+                          style: CupertinoTheme.of(context)
+                              .textTheme
+                              .tabLabelTextStyle
+                              .merge(const TextStyle(color: CupertinoColors.label)),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Date:',
+                          style: CupertinoTheme.of(context)
+                              .textTheme
+                              .textStyle
+                              .merge(TextStyle(color: CupertinoColors.label.withOpacity(.5))),
+                        ),
+                        Text(
+                          'Lorem Ipsum is simply dummy ',
+                          style: CupertinoTheme.of(context)
+                              .textTheme
+                              .tabLabelTextStyle
+                              .merge(const TextStyle(color: CupertinoColors.label)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
